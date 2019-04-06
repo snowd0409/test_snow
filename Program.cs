4ebc3fd60a8c9a7a7e43105b732e1e5cc3be1cc2
Toan Duong Ngoc <tt0409@gmail.com>
2019-04-06 09:24:35 +0700

    Initial check-in of module test_snow

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace test_snow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
