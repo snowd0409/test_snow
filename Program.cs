f727537bb1c7e827f428289029a513f0fd60801d
cafe <toanduong@outlook.com>
2019-04-06 20:59:37 +0700

    xccode lan 2

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace test_snow
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!,i'm new version 1.0.1.2 from xCode");
+            Console.WriteLine("Hello World!,i'm new version 1.0.1.2 from xCode - xCode lan 2");
         }
     }
 }
